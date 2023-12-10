8d27034072d85db1002f946d0ad7d60d6530269c
Jana Marie <janamarie@janamarie.dev>
2023-12-10 11:33:36 +0100

    added different display typeas

--- common_code-different_roots/gen/ccdr.py
+++ common_code-different_roots/gen/ccdr.py
@@ -3,6 +3,53 @@
 
 import time, random, sys, math
 import cairo
+# 532nm green
+background_color = (0.165, 0.165, 0.165)
+led_on_color = (0.0, 0.996, 0.71)
+led_off_color = (0.16, 0.3, 0.29)
+segment_bg_color = (0.1, 0.2, 0.19)
+segment_line_color = (0.408, 0.52, 0.51)
+filename = "gpn22_ccdr_vfd_532.svg"
+root_str = [[0, 0, ""]]
+seed = ["alerta", "anti", "fascista~"]
+start = [2, 11, 15]
+'''
+
+# Nixie red
+background_color = (0.165, 0.165, 0.165)
+led_on_color = (0.988, 0.941, 0.365) # glow color #7F160B
+led_off_color = (0.498, 0.086, 0.043)
+segment_bg_color = (0.2, 0.153, 0.149) #(0.196, 0.031, 0.016)
+segment_line_color = (0.408, 0.52, 0.51)
+filename = "gpn22_ccdr_vfd_nixie.svg"
+root_str = [[0, 0, ""]]
+seed = ["g", "p", "N"]
+start = [2, 8, 15]
+'''
+'''
+# Terminal Amber
+background_color = (0.165, 0.165, 0.165)
+led_on_color = (0.984, 0.984, 0.157)
+led_off_color = (0.251, 0.161, 0.016)
+segment_bg_color = (0.125, 0.078, 0.047)
+segment_line_color = (0.408, 0.52, 0.51)
+filename = "gpn22_ccdr_vfd_amber.svg"
+root_str = [[0, 0, ""]]
+seed = ["FUCK", "FRONTEX", "!"]
+start = [3, 6, 14]
+'''
+'''
+# Terminal Yellow
+background_color = (0.165, 0.165, 0.165)
+led_on_color = (0.651, 0.914, 0.961)
+led_off_color = (0.192, 0.271, 0.268)
+segment_bg_color = (0.063, 0.090, 0.094)
+segment_line_color = (0.408, 0.52, 0.51)
+filename = "gpn22_ccdr_vfd_blue.svg"
+root_str = [[0, 0, ""]]
+seed = ["mau", "meow", "nya"]
+start = [6, 11, 16]
+'''
 
 # nya nya mau mau mau! :3 ~jana
 
@@ -12,14 +59,14 @@ def draw_14_seg_disp(ctx, x, y, scale, col, character):
 
 	# rect
 	ctx.rectangle(x, y, xn, yn)
-	ctx.set_source_rgb(0.1, 0.2, 0.19)
+	ctx.set_source_rgb(*segment_bg_color)
 	ctx.fill()
-	ctx.set_source_rgb(0.408, 0.52, 0.51)
+	ctx.set_source_rgb(*segment_line_color)
 	ctx.set_line_width(0.2)
 	ctx.stroke()
 
 	# ~ (background, unlit segments)
-	ctx.set_source_rgb(0.16, 0.3, 0.29)
+	ctx.set_source_rgb(*led_off_color)
 	ctx.set_font_size(scale*0.7)
 	ctx.select_font_face("DSEG14 Modern",
 					 cairo.FONT_SLANT_NORMAL,
@@ -51,19 +98,16 @@ def draw_14_seg_disp(ctx, x, y, scale, col, character):
 # nya
 print("Generating Display")
 WIDTH, HEIGHT = 1000, 1414
-surface = cairo.SVGSurface("gpn22_ccdr.svg", WIDTH, HEIGHT)
+surface = cairo.SVGSurface(filename, WIDTH, HEIGHT)
 ctx = cairo.Context(surface)
 ctx.scale(HEIGHT, HEIGHT)
 
 # BG
 ctx.rectangle(0, 0, 1, 1)
-ctx.set_source_rgb(0.165, 0.165, 0.165)
+ctx.set_source_rgb(*background_color)
 ctx.fill()
 
 # lets generate some roots
-root_str = [[0, 0, ""]]
-seed = ["alerta", "anti", "fascista~"]
-start = [2, 11, 15]
 # "char", x rule, [y rules, follow]
 rules = {	"|" : [1, [[0, "YYYYYYYYYYYY\\\\//()()| "]]],
 			"Y" : [1, [[1, "\\\\\\)))||| '"],
@@ -96,7 +140,7 @@ for i in range(len(start)):
 			head.append(new_root)
 			root_str.append(new_root)
 		
-		if(new_y > 8 or head == []):
+		if(new_y > 7 or head == []):
 			alive = False
 
 
@@ -137,16 +181,16 @@ _x,_y,_scale = 20, 18, 0.05
 
 for x in range(_x):
 	for y in range(_y):
-		draw_14_seg_disp(ctx, 0.707108562/_x*x, 1/(_y+2)*y, _scale, (0.0, 0.996, 0.71), " ")
+		draw_14_seg_disp(ctx, 0.707108562/_x*x, 1/(_y+2)*y, _scale, led_on_color, " ")
 		for i in range(len(ccdr_str)):
 			if(x == ccdr_str[i][0] and y == ccdr_str[i][1]):
-				draw_14_seg_disp(ctx, 0.707108562/_x*x, 1/(_y+2)*y, _scale, (0.0, 0.996, 0.71), ccdr_str[i][2])
+				draw_14_seg_disp(ctx, 0.707108562/_x*x, 1/(_y+2)*y, _scale, led_on_color, ccdr_str[i][2])
 
 # write GPN-22
 gpn22_str = ["G", "P", "N", "-", "2", "2"]
 
 for x in range(len(gpn22_str)):
-	draw_14_seg_disp(ctx, 0.707108562/_x*(x*3)+0.707108562/_x, 1/(_y+2)*10, _scale*3, (0.0, 0.996, 0.71), gpn22_str[x]) #0.707108562/10*x+0.707108562/10*2.5
+	draw_14_seg_disp(ctx, 0.707108562/_x*(x*3)+0.707108562/_x, 1/(_y+2)*10, _scale*3, led_on_color, gpn22_str[x]) #0.707108562/10*x+0.707108562/10*2.5
 
 # footer - this does just not work, wtf
 '''
