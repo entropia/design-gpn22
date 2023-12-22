f2a5839d40f2609ac65e8cd8fee9d909199f1574
Jana Marie <janamarie@janamarie.dev>
2023-12-22 11:33:02 +0100

    added intro animator

--- common_code-different_roots/gen/animator.py
+++ common_code-different_roots/gen/animator.py
@@ -5,9 +5,15 @@ from gpn22_ccdr_gen import design
 import sys, math, random
 from colour import Color
 
+# made in agony (no, actaully it was fun)
+
 FPS = 30
 SEC = 10
 frames = SEC*FPS
+
+#
+# build keyframes
+#
 
 # [frame, action, values]
 keyframes = []
@@ -29,18 +35,20 @@ hb_low_color = Color(rgb=(0.0501, 0.8614, 0.6559))
 hb_blur_center = 40
 hb_blur_jitter = 3
 hb_color_shift_len = 100
-hb_color_grad = list(hb_high_color.range_to(hb_low_color, int(hb_color_shift_len/2))) + list(hb_low_color.range_to(hb_high_color, int(hb_color_shift_len/2)))
+hb_color_grad = list(hb_high_color.range_to(hb_low_color, int(hb_color_shift_len/2))) + 
+				list(hb_low_color.range_to(hb_high_color, int(hb_color_shift_len/2)))
 
 for i in range(hb_frames):
-	keyframes.append([i+hb_start, "set_segment_color", tuple(map(lambda x: round(x, 3), hb_color_grad[i % hb_color_shift_len].rgb))])
+	keyframes.append([i+hb_start, "set_segment_color", 
+						tuple(map(lambda x: round(x, 3), hb_color_grad[i % hb_color_shift_len].rgb))])
 	keyframes.append([i+hb_start, "set_segment_blur", (math.sin(i/100)*hb_blur_jitter)+hb_blur_center])
 
 # roots
-root_start = 250
-root_advance = 1
+root_start = 220
+root_advance = 3
 root_max = 20
 root_cnt = 0
-root_sp = [7,3,11,23,12,26,16,14,17,25,19,15,1,4,18,27,22,5,0,24,6,13,8,2,9,20,21,10,22,14, 9, 5,24,20, 1,21,18,13,26,19, 8, 3, 2,25,10,27,12,15, 4, 6,16,11, 7,23, 0,17]
+root_sp = [7,3,11,23,12,26,16,14,17,25,19,15,1,4,18,27,22,5,0,24,6,13,8,2,9,20,21,10,22,14,9,5,24,20,1,21,18,13,26,19,8,3,2,25,10,27,12,15,4,6,16,11,7,23,0,17]
 roots_pre = []
 roots = []
 
@@ -61,6 +69,24 @@ for i in range(on_off_frames):
 	keyframes.append([289+i, "set_1_color", tuple(map(lambda x: round(x, 3), on_to_off_1[i].rgb))])
 	keyframes.append([289+i, "set_2_color", tuple(map(lambda x: round(x, 3), on_to_off_2[i].rgb))])
 
+#
+# render SVGs based on keyframes
+#
+# afterwards convert SVGs to pngs via
+#
+'''
+	#!/bin/bash
+	for file in *.svg; do
+	    filename=$(basename "$file")
+	    inkscape "$file" --export-type=png --export-width=1920 --export-height=1080 --export-area=0:136:1885:1196 -o "png/${filename%.svg}.png"
+	done
+'''
+# 
+# and convert pngs to gif via
+#
+# gifski -r 30 -W 1920 -H 1080 -o gpn22_congress_infobeamer_1080p_final.gif congress_info*.png
+#
+
 if __name__ == "__main__":
 	
 	for n in range(frames):
@@ -68,6 +94,7 @@ if __name__ == "__main__":
 		gpn22.filename = "congress_info" + str(n) + ".svg"
 		gpn22.outdir = "out/anim/"
 		gpn22.create_surface()
+
 		for kf in keyframes:
 			if kf[0] == n:
 				if kf[1] == "set_segment_color":
@@ -89,11 +116,12 @@ if __name__ == "__main__":
 
 		if roots_pre:
 			for r in roots_pre:
-				if n > r[0]:
-					for c in r[2]:
-						if r[1] == c[1]:
-							roots.append(c)
-					r[1] += 1
+				if n % 2 == 0:
+					if n > r[0]:
+						for c in r[2]:
+							if r[1] == c[1]:
+								roots.append(c)
+						r[1] += 1
 
 
 		gpn22.draw_bg()
